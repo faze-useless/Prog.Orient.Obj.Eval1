8c3a2e27623fc938c27b75f7f14a48d88173d998
faze-useless <143573781+faze-useless@users.noreply.github.com>
2024-02-02 18:50:33 -0600

    Add files via upload

---
+++ EVA1_1_CLASES_OBJETOS/src/eva1_1_clases_objetos/EVA1_1_CLASES_OBJETOS.java
@@ -0,0 +1,24 @@
+package eva1_1_clases_objetos;
+
+/**
+ *
+ * @author DELL
+ */
+public class EVA1_1_CLASES_OBJETOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Humano human1; // variable tipo humano
+        //crear el objeto (instanciar) --> asignar memoria al objeto
+        int value;
+    }
+    
+}
+
+//clase pipol
+//Nombre de la clase --> nomenclatura CamelCase
+class Humano {
+}
